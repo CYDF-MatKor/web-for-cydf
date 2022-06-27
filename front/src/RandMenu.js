e00605180aad71989b886d2fc4f275a8020a4739
kidw0124 <kidw0124@naver.com>
2022-06-28 05:49:54 +0900

    RandMenu페이지 생성

---
+++ front/src/RandMenu.js
@@ -0,0 +1,10 @@
+import "./RandMenu.css";
+function RandMenu(props) {
+	return (
+		<div className="RandMenu-page">
+			<h1>RandMenu</h1>
+			<p>랜덤 메뉴</p>
+		</div>
+	);
+}
+export default RandMenu;
