7d3688c1112d4ba2c587c66808b47ad16a697b2c
kidw0124 <kidw0124@naver.com>
2022-06-28 04:43:15 +0900

    common 페이지 라우팅

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,92 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
+import { About, Contact, Error, Terms } from ".";
 
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const [isPaid, setIsPaid] = useState(false);
 	return (
 		<div className="App">
-			<div id="navbar"></div>
-			<Routes id="page">
-				{/* <Route path="/" element={<Home />} />
+			<div id="navbar">Navbar</div>
+			<div id="page">
+				<Routes id="page-login-common">
 					<Route path="/about" element={<About />} />
-					<Route path="/contact" element={<Contact />} /> */}
-			</Routes>
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/error" element={<Error />} />
+					<Route path="/terms" element={<Terms />} />
+				</Routes>
+				{/* {isLoggedIn ? (
+					<Routes id="page-login-true">
+						<Route path="/" element={<Home />} />
+						<Route path="/home" element={<Home />} />
+						<Route path="/mypage" element={<Mypage />} />
+						<Route path="/timetable" element={<Timetable />} />
+						<Route path="/grade" element={<Grade />} />
+						<Route
+							path="/lecture/description"
+							element={<Lecture />}
+						/>
+						{isLoggedIn && (
+							<Route
+								path="/lecture/:num/info"
+								element={<LectureInfo />}
+							/>
+						)}
+						{isLoggedIn && (
+							<Route
+								path="/lecture/:num/pastexam"
+								element={<LecturePastexam />}
+							/>
+						)}
+						{isLoggedIn && (
+							<Route
+								path="/lecture/:num/tier"
+								element={<LectureTier />}
+							/>
+						)}
+						{isLoggedIn && (
+							<Route
+								path="/professor/:num"
+								element={<Professor />}
+							/>
+						)}
+						{isLoggedIn && (
+							<Route
+								path="/professor/:num/info"
+								element={<ProfessorInfo />}
+							/>
+						)}
+						{isLoggedIn && (
+							<Route
+								path="/professor/:num/tier"
+								element={<ProfessorTier />}
+							/>
+						)}
+						{isLoggedIn && (
+							<Route
+								path="/academic-calendar/:year"
+								element={<AcademicCalendar />}
+							/>
+						)}
+						{isLoggedIn && (
+							<Route
+								path="/부사수/:year/:semester"
+								element={<Substitute />}
+							/>
+						)}
+						<Route path="/rand-menu" element={<RandMenu />} />
+						<Route path="/club/:club" element={<Club />} />
+					</Routes>
+				) : (
+					<Routes id="page-login-false">
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route path="/*" element={<Navigate to="/login" />} />
+					</Routes>
+				)} */}
+			</div>
 		</div>
 	);
 }
