13e73f03cf380f374fa4db87fcbfc1389282d6d2
kidw0124 <kidw0124@naver.com>
2022-06-28 04:17:25 +0900

    About페이지

---
+++ front/src/About.js
@@ -0,0 +1,15 @@
+import "./About.css";
+
+function About() {
+	return (
+		<div className="About-page">
+			<h1>About</h1>
+			<p>
+				이 사이트는 할일없을줄알았던 20, 21, 22학번 응애들이
+				만들었습니다.
+			</p>
+		</div>
+	);
+}
+
+export default About;
