bc69657fb7ea061472f70c49f68f81f3a3d7701b
kidw0124 <kidw0124@naver.com>
2022-06-28 05:39:46 +0900

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -19,8 +19,8 @@ import {
 } from ".";
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [isPaid, setIsPaid] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(true);
+	const [isPaid, setIsPaid] = useState(true);
 	return (
 		<div className="App">
 			<div id="navbar">Navbar</div>
@@ -31,49 +31,47 @@ function App() {
 					<Route path="/error" element={<Error />} />
 					<Route path="/terms" element={<Terms />} />
 				</Routes>
-				{/* {isLoggedIn ? (
+				{isLoggedIn ? (
 					<Routes id="page-login-true">
-						<Route path="/" element={<Home />} />
+						{/* <Route path="/" element={<Home />} />
 						<Route path="/home" element={<Home />} />
 						<Route path="/mypage" element={<Mypage />} />
 						<Route path="/timetable" element={<Timetable />} />
-						<Route path="/grade" element={<Grade />} />
+						<Route path="/grade" element={<Grade />} /> */}
 						<Route
 							path="/lecture/description"
 							element={<Lecture />}
 						/>
-						{isLoggedIn && (
+						{isPaid && (
 							<Route
 								path="/lecture/:num"
 								element={<LectureInfo />}
 							/>
 						)}
-						{isLoggedIn && (
+						{isPaid && (
 							<Route
 								path="/lecture/pastexam/:num"
 								element={<LecturePastexam />}
 							/>
 						)}
-						{isLoggedIn && (
+						{isPaid && (
 							<Route
 								path="/lecture/tier"
 								element={<LectureTier />}
 							/>
 						)}
-						{isLoggedIn && (
+						{isPaid && (
 							<Route
 								path="/professor/:num"
-						{isLoggedIn && (
 								element={<ProfessorInfo />}
 							/>
 						)}
-						{isLoggedIn && (
+						{isPaid && (
 							<Route
 								path="/professor/tier"
 								element={<ProfessorTier />}
 							/>
 						)}
-						{isLoggedIn && (
 						<Route
 							path="/academic-calendar/:year"
 							element={<AcademicCalendar />}
@@ -82,32 +80,33 @@ function App() {
 							path="/academic-calendar"
 							element={<Navigate to="/academic-calendar/2022" />}
 						/>
+						{isPaid && (
 							<Route
 								path="/%EB%B6%80%EC%82%AC%EC%88%98/:year/:semester"
 								element={<Busasu />}
 							/>
 						)}
-						{isLoggedIn && (
+						{isPaid && (
 							<Route
 								path="/부사수/:year/:semester"
 								element={<Busasu />}
 							/>
 						)}
-						<Route path="/rand-menu" element={<RandMenu />} />
+						{isPaid && (
 							<Route
 								path="/busasu/:year/:semester"
 								element={<Busasu />}
 							/>
 						)}
+						{/* <Route path="/rand-menu" element={<RandMenu />} /> */}
 						<Route path="/club/:club" element={<Club />} />
 					</Routes>
-				) : (
-					<Routes id="page-login-false">
-						<Route path="/login" element={<Login />} />
-						<Route path="/signup" element={<Signup />} />
-						<Route path="/*" element={<Navigate to="/login" />} />
-					</Routes>
-				)} */}
+				) : // <Routes id="page-login-false">
+				// 	<Route path="/login" element={<Login />} />
+				// 	<Route path="/signup" element={<Signup />} />
+				// 	<Route path="/*" element={<Navigate to="/login" />} />
+				// </Routes>
+				null}
 			</div>
 		</div>
 	);
