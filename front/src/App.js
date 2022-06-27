42ae840ae712cc95dc7a1086640c2bd3e75cbba9
kidw0124 <kidw0124@naver.com>
2022-06-28 04:05:00 +0900

    로그인여부, 학생회비 여부 설정

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isPaid, setIsPaid] = useState(false);
 	return (
 		<div className="App">
 			<div id="navbar"></div>
