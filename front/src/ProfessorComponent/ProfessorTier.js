2659981baa14d0870fd664dbb0084de074f6bef9
kidw0124 <kidw0124@naver.com>
2022-06-28 05:18:00 +0900

    ProfessorTier페이지 생성

---
+++ front/src/ProfessorComponent/ProfessorTier.js
@@ -0,0 +1,10 @@
+import "./ProfessorTier.css";
+function ProfessorTier(props) {
+	return (
+		<div className="ProfessorTier-page">
+			<h1>ProfessorTier</h1>
+			<p>티어</p>
+		</div>
+	);
+}
+export default ProfessorTier;
