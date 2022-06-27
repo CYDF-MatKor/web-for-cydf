f28ebc264072c49eb92fcc62274b98e93f4171c5
kidw0124 <kidw0124@naver.com>
2022-06-28 05:13:31 +0900

    LectureTier 생성

---
+++ front/src/LectureComponent/LectureTier.js
@@ -0,0 +1,10 @@
+import "./LectureTier.css";
+function LectureTier(props) {
+	return (
+		<div className="LectureTier-page">
+			<h1>LectureTier</h1>
+			<p>티어</p>
+		</div>
+	);
+}
+export default LectureTier;
