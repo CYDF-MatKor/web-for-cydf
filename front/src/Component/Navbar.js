8cd3da1a5c6be5eea40bb9fcb8279467a5342b1f
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-07-08 02:35:27 +0900

    Make Navbar div structure and left logo

--- front/src/Component/Navbar.js
+++ front/src/Component/Navbar.js
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import icon from "./navbaricon.png";
 
 function Navbar() {
-	return <div id="navbar"></div>;
+	return (
+		<div id="navbar">
+			<div id="navbar-main">
+				<div id="navbar-main-left">
+					<Link id="navbar-logo-link" to="/">
+						<img id="navbar-logo-img" src={icon} alt="logo" />
+					</Link>
+				</div>
+				<div id="navbar-main-middle"></div>
+				<div id="navbar-main-right"></div>
+			</div>
+			<div id="navbar-sub">
+				<div id="navbar-sub-left"></div>
+				<div id="navbar-sub-middle"></div>
+				<div id="navbar-sub-right"></div>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
