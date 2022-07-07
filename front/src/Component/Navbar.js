6c29a73ecb9aa1ba93447338aae1ae2a10e209ee
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-07-08 03:04:17 +0900

    Navbar subpanel

--- front/src/Component/Navbar.js
+++ front/src/Component/Navbar.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import icon from "./navbaricon.png";
 
 function Navbar({ isLoggedIn, isPaid }) {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
-		<div id="navbar">
+		<div
+			id="navbar"
+			onMouseLeave={() => {
+				setIsOpen(false);
+			}}
+		>
 			<div id="navbar-main">
 				<div id="navbar-main-left">
 					<Link id="navbar-logo-link" to="/">
 						<img id="navbar-logo-img" src={icon} alt="logo" />
 					</Link>
 				</div>
-				<div id="navbar-main-middle">
+				<div id="navbar-main-middle" onClick={() => setIsOpen(true)}>
 					<Link className="navbar-main-middle-link" to="/">
 						소개
 					</Link>
@@ -40,7 +48,7 @@ function Navbar({ isLoggedIn, isPaid }) {
 					)}
 				</div>
 			</div>
-			<div id="navbar-sub">
+			<div id="navbar-sub" style={isOpen ? {} : { height: 0 }}>
 				<div id="navbar-sub-left"></div>
 				<div id="navbar-sub-middle"></div>
 				<div id="navbar-sub-right"></div>
